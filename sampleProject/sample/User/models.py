f195f4844494ff8ad79dd34b5e31fffb8662534c
sayanth123 <59867917+sayanth123@users.noreply.github.com>
2021-07-06 11:38:06 +0530

    Update models.py

--- sampleProject/sample/User/models.py
+++ sampleProject/sample/User/models.py
@@ -3,14 +3,7 @@ from django.conf import settings
 from django.contrib.auth.models import User
 from .forms import RegisterForm
 
-# class UserProfile(models.Model):
-#     user = models.OneToOneField(User, on_delete=models.CASCADE)
-#     email=models.EmailField(max_length=200,unique=True)
-#     first_name=models.CharField(max_length=60)
-#     last_name=models.CharField(max_length=30)
-#
-#     def __str__(self):
-#         return self.user
+
 
 class Post(models.Model):
     text=models.TextField()
